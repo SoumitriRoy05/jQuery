c275c94708d8d484ad154553dad8d36556f1b615
SOUMITRI ROY <soumitriroy05@gmail.com>
2025-02-09 19:47:59 +0530

    Add files via upload

---
+++ Selecting_elements_jQuery/index.js
@@ -0,0 +1,8 @@
+// document.querySelector("h1");
+// console.log($("h1").css("font-size"));
+$("h1").css("color");
+$("h1").css("font-size","5rem");
+$("button");
+$("h1").addClass("big-title margin-50");
+$("h1").removeClass("big-title");
+$("h1").hasClass("margin-50");
